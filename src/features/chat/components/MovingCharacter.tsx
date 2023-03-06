dd2cf88667d1ce89d0157354c78f8bac79af08fd
yuki <shimazuyuki7@gmail.com>
2023-03-06 13:25:56 +0900

    動き小さく

--- src/features/chat/components/MovingCharacter.tsx
+++ src/features/chat/components/MovingCharacter.tsx
@@ -7,15 +7,15 @@ const imageUrl =
 
 const animationKeyframes = keyframes`
 0% { transform: translate(0, 0) rotate(0deg); }
-10% { transform: translate(-10px, 0) rotate(-5deg); }
-20% { transform: translate(10px, 0) rotate(5deg); }
-30% { transform: translate(-10px, 0) rotate(-5deg); }
-40% { transform: translate(10px, 0) rotate(5deg); }
-50% { transform: translate(-10px, 0) rotate(-5deg); }
-60% { transform: translate(10px, 0) rotate(5deg); }
-70% { transform: translate(-10px, 0) rotate(-5deg); }
-80% { transform: translate(10px, 0) rotate(5deg); }
-90% { transform: translate(-10px, 0) rotate(-5deg); }
+10% { transform: translate(-5px, 0) rotate(-5deg); }
+20% { transform: translate(5px, 0) rotate(5deg); }
+30% { transform: translate(-5px, 0) rotate(-5deg); }
+40% { transform: translate(5px, 0) rotate(5deg); }
+50% { transform: translate(-5px, 0) rotate(-5deg); }
+60% { transform: translate(5px, 0) rotate(5deg); }
+70% { transform: translate(-5px, 0) rotate(-5deg); }
+80% { transform: translate(5px, 0) rotate(5deg); }
+90% { transform: translate(-5px, 0) rotate(-5deg); }
 100% { transform: translate(0, 0) rotate(0deg); }
 `;
 
